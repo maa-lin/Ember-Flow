ac63414058977ba7260f7d1e02ae1a6a08266237
Malin <maa.lin@outlook.com>
2025-12-29 11:51:20 +0100

    feat: Add breathing animation to the breathe-modal.

--- frontend/src/components/BreatheModal/BreatheModal.tsx
+++ frontend/src/components/BreatheModal/BreatheModal.tsx
@@ -1,7 +1,17 @@
+import { useContext } from "react"
+import { BreathingAnimation } from "../BreathingAnimation/BreathingAnimation"
 import { CloseButton } from "../CloseButton/CloseButton"
 import styles from "./BreatheModal.module.scss"
+import { MoodContext } from "../../contexts/MoodContext"
+import { Navigate } from "react-router"
 
 export const BreatheModal = () => {
+
+    const moodContext = useContext(MoodContext);
+
+    if (!moodContext) {
+        <Navigate to={"/mood"} replace />
+    }
 
     return <div className={styles["breathe-modal"]}>
         <CloseButton />
@@ -9,5 +19,6 @@ export const BreatheModal = () => {
         <p>Breathe in as the circle grows,<br/>
             breathe out as it softens again.</p>
         <p>Take as much or as little time as you need.</p>
+        <BreathingAnimation />
     </div>
 }
---
+++ frontend/src/components/BreathingAnimation/BreathingAnimation.tsx
@@ -0,0 +1,26 @@
+import { motion } from "motion/react";
+import styles from "./BreathingAnimation.module.scss";
+
+export const BreathingAnimation = () => {
+
+    return <div className={styles["breathe-container"]}>
+        {[...Array(5)].map((_, i) => (
+            <motion.span
+                key={i}
+                className={styles.ring}
+                animate={{
+                    scale: [1 + i * 0.15, 1.8 + i * 0.8, 1 + i * 0.15]
+                }}
+                transition={{
+                    duration: 12,
+                    repeat: Infinity,
+                    ease: "easeInOut",
+                    delay: i * 0.2
+                
+                }}
+            >
+            </motion.span>
+        ))}
+        <div className={styles.core}></div>
+    </div>
+}
