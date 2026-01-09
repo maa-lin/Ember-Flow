4d1e31cd8925bdb0290e4beed929fa8b94bfe7f2
Malin <maa.lin@outlook.com>
2026-01-09 15:08:38 +0100

    change module from commonjs to esnext

---
+++ backend/dist/models/ChallengeSchema.mjs
@@ -0,0 +1,22 @@
+"use strict";
+import { model, Schema } from "mongoose";
+const ChallengeSchema = new Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    mood: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    affirmation: {
+        type: String,
+        required: true
+    },
+});
+const Challenge = model("Challenge", ChallengeSchema);
+export default Challenge;
